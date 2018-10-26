ebedc3234c808dda4eb5f7dbd9145cca71a3e037
sanjeev <sanjeevh.93@gmail.com>
2018-10-26 21:27:47 +0530

    updated node names

--- cordapp/src/main/kotlin/net/corda/yo/Client.kt
+++ cordapp/src/main/kotlin/net/corda/yo/Client.kt
@@ -10,24 +10,24 @@ fun main(args: Array<String>) {
 }
 
 private class YoRPC {
-    companion object {
-        val logger: Logger = loggerFor<YoRPC>()
-    }
+//    companion object {
+//        val logger: Logger = loggerFor<YoRPC>()
+//    }
 
     fun main(args: Array<String>) {
-        require(args.size == 1) { "Usage: YoRPC <node address:port>" }
-        val nodeAddress = NetworkHostAndPort.parse(args[0])
-        val client = CordaRPCClient(nodeAddress)
-        // Can be amended in the com.template.MainKt file.
-        val proxy = client.start("user1", "test").proxy
-        // Grab all signed transactions and all future signed transactions.
-    //    val (transactions, futureTransactions) = proxy.in
-        // Log the existing Yo's and listen for new ones.
-//        futureTransactions.startWith(transactions).toBlocking().subscribe { transaction ->
-//            transaction.tx.outputs.forEach { output ->
-//                val state = output.data as YoState
-//                logger.info(state.toString())
-//            }
-//        }
+//        require(args.size == 1) { "Usage: YoRPC <node address:port>" }
+//        val nodeAddress = NetworkHostAndPort.parse(args[0])
+//        val client = CordaRPCClient(nodeAddress)
+//        // Can be amended in the com.template.MainKt file.
+//        val proxy = client.start("user1", "test").proxy
+//        // Grab all signed transactions and all future signed transactions.
+//    //    val (transactions, futureTransactions) = proxy.in
+//        // Log the existing Yo's and listen for new ones.
+////        futureTransactions.startWith(transactions).toBlocking().subscribe { transaction ->
+////            transaction.tx.outputs.forEach { output ->
+////                val state = output.data as YoState
+////                logger.info(state.toString())
+////            }
+////        }
     }
 }
