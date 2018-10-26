dcebfad1846f9536acd617a747bd7cf930c9f861
sanjeev <sanjeevh.93@gmail.com>
2018-10-26 18:28:09 +0530

    Added docker and run configurations for application deployment

---
+++ api/run-backend.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If variable not present use default values
+: ${APP_HOME:=/opt/app/src/build/libs/}}
+: ${JAVA_OPTIONS:=-Xmx512m}
+
+export CORDA_HOME JAVA_OPTIONS
+
+cd ${APP_HOME}
+
+java $JAVA_OPTIONS -Dspring.profiles.active=${SPRING_PROFILE} -jar corda-api-0.0.1-SNAPSHOT.jar
